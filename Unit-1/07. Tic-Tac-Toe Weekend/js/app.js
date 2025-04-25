44b5d5090c8fd55907cf86a77112aecaccc6b51f
jbarron5925 <ant.baroon5925@gmail.com>
2025-04-25 09:56:55 -0500

    establish constraints and state variables

--- Unit-1/07. Tic-Tac-Toe Weekend/js/app.js
+++ Unit-1/07. Tic-Tac-Toe Weekend/js/app.js
@@ -1,10 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
+const squareELs = document.querySelectorAll('.sqr');
+const messageEl = document.getElementById('message');
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+let board;
+let turn;
+let winner;
+let tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -18,3 +20,17 @@
 
 
 
+//1) Define the required variables used to track the state of the game.
+
+//2) Store cached element references.
+
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+//5) Define the required constants.
+
+//6) Handle a player clicking a square with a `handleClick` function.
+
+//7) Create Reset functionality.
