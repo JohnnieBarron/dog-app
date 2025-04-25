05cb9d39412058265c456ff06f02196b2e32e25a
jbarron5925 <ant.baroon5925@gmail.com>
2025-04-25 11:55:53 -0500

    complete winning combos, render message, update render fuction

--- Unit-1/07. Tic-Tac-Toe Weekend/js/app.js
+++ Unit-1/07. Tic-Tac-Toe Weekend/js/app.js
@@ -1,6 +1,20 @@
 /*-------------------------------- Constants --------------------------------*/
-const squareELs = document.querySelectorAll('.sqr');
-const messageEl = document.querySelector('h2');
+
+const PLAYERS = {
+    '1': 'X',
+    '-1': 'O',
+  };
+
+  const winningCombos = [
+    [0, 1, 2], // top row
+    [3, 4, 5], // middle row
+    [6, 7, 8], // bottom row
+    [0, 3, 6], // left column
+    [1, 4, 7], // middle column
+    [2, 5, 8], // right column
+    [0, 4, 8], // top-left to bottom-right diagonal
+    [2, 4, 6]  // top-right to bottom-left diagonal
+  ];
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board;
@@ -10,12 +24,15 @@ let tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squareELs = document.querySelectorAll('.sqr');
+const messageEl = document.querySelector('h2');
 
 
 /*-------------------------------- Functions --------------------------------*/
 
 function init () {
-    board = [null, null, null, null, null, null, null, null, null];
+    board = 
+    [null, null, null, null, null, null, null, null, null];
     turn = (1);
     winner = (null);
     tie = ('Tie');
@@ -23,25 +40,52 @@ function init () {
     render();
 };
 
-function render() {
+function handleClick(Event) {
 
+};
+
+function updateBoard() {
+    board.forEach((value, index) => {
+        if (value === 1) {
+            squareELs[index].textContent = 'X';
+        } else if (value === -1) {
+            squareELs[index].textContent = 'O';
+        } else {
+            squareELs[index].textContent = '';
+        }
+    });
+}
+
+function renderMessage() {
+    if (winner === '1') {
+        messageEl.textContent = " X's Win!";
+    } else if (winner === '-1') {
+        messageEl.textContent = " O's Win!";
+    } else if (winner === 'Tie') {
+        messageEl.textContent = " Tie Game!";}
+    // } else if (winner === null) {
+    //     messageEl.textContent = `${PLAYERS}'s Turn`;
+    // }
+};
+
+function render() {
+    updateBoard();
+    renderMessage();
+    console.log('render')
 };
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
+squareELs.addEventListener('click', handleClick);
 
-//1) Define the required variables used to track the state of the game.
 
-//2) Store cached element references.
 
-//3) Upon loading, the game state should be initialized, and a function should 
-//   be called to render this game state.
 
-//4) The state of the game should be rendered to the user.
 
-//5) Define the required constants.
+
+
 
 //6) Handle a player clicking a square with a `handleClick` function.
 
